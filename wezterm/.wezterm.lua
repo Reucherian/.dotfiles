7af091dce08bc53d3f2a6677bfa7c82c5285cf1f
Reuben Cherian <cherianr@mskcc.org>
2024-07-10 14:09:20 -0400

    wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require 'wezterm'
+return {
+	color_scheme = 'Catppuccin Mocha',
+	enable_tab_bar = false,
+	font_size = 14.0,
+	font = wezterm.font("MonaspiceNe Nerd Font", {weight="Medium", stretch="Normal", style="Normal"}),
+	harfbuzz_features = {"calt", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "ss09", "liga" }, -- enabling all the ligatures and fonts that wezterm will use
+	macos_window_background_blur = 30, -- macos background blur
+	window_background_opacity = 0.95, -- window opacity
+	window_decorations = 'RESIZE', -- removes the window title bar and allows resize
+	keys = { -- keybinding modifiers
+		{ key = 'f', mods = 'CTRL',action = wezterm.action.ToggleFullScreen }, -- might remove
+		{ key = 'k', mods = 'CMD', action = wezterm.action.SendString '\x0c' }, -- to clear scrollback from view
+		{ key = 'LeftArrow', mods = 'OPT', action = wezterm.action.SendString '\x1bb' }, -- for alt left to move word left
+ 		{ key = 'RightArrow', mods = 'OPT', action = wezterm.action.SendString '\x1bf' }, -- for alt right move word right
+	},
+	mouse_bindings = { -- mouse bindings
+	  { event = { Up = { streak = 1, button = 'Left' } }, mods = 'CTRL', action = wezterm.action.OpenLinkAtMouseCursor }, -- ctrl-click will open the link under the mouse cursor
+	},
+}
