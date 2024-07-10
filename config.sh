e423fbf7d72a5bb9abfbc475cb8f4d43e513a6b9
Reuben Cherian <cherianr@mskcc.org>
2024-07-10 14:09:45 -0400

    wezterm

--- config.sh
+++ config.sh
@@ -4,6 +4,7 @@ if [ -d $HOME/.dotfiles ]; then
     stow zsh
     stow starship
     stow tmux
+    stow wezterm
 else
     echo "$HOME/.dotfiles does not exist.\n clone to the home folder instead"
 fi
