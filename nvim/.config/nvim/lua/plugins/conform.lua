3e93ef77444af3a004483ec94a716fe344019e2d
Reuben Cherian <cherianr@mskcc.org>
2024-09-08 11:00:30 -0400

    changing linting and formatting tools

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatter_by_ft = {
+        python = { "isort", "black" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      }
+    })
+    vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500
+      })
+    end, { desc = "Format file or range" })
+  end
+}
