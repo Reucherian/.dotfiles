1e2ea1232552c67ee7b325c95dd4ac62296d1d3f
Reuben Cherian <cherianr@mskcc.org>
2024-09-08 10:26:16 -0400

    adding icons to lsp hints

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,40 +1,47 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "pyright", "gopls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local nvim_lsp = require("lspconfig")
-			nvim_lsp.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			nvim_lsp.tsserver.setup({
-				capabilities = capabilities,
-			})
-			nvim_lsp.gopls.setup({
-				capabilities = capabilities,
-			})
-			vim.api.nvim_create_autocmd("LspAttach", {
-				callback = function()
-					vim.keymap.set("n", "grn", vim.lsp.buf.rename)
-					vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
-					vim.keymap.set("n", "grr", vim.lsp.buf.references)
-					vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
-				end,
-			})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        PATH = "append"
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "pyright", "gopls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
+      local nvim_lsp = require("lspconfig")
+      nvim_lsp.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      nvim_lsp.tsserver.setup({
+        capabilities = capabilities,
+      })
+      nvim_lsp.gopls.setup({
+        capabilities = capabilities,
+      })
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function()
+          vim.keymap.set("n", "grn", vim.lsp.buf.rename)
+          vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
+          vim.keymap.set("n", "grr", vim.lsp.buf.references)
+          vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
+        end,
+      })
+    end,
+  },
 }
