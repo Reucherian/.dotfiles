cbeb541e381706ddcb91ff9436bcffafff84752b
Reuben Cherian <reuben.cherian@icloud.com>
2025-04-09 10:20:47 -0400

    configure telescope to use opts

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,23 +3,24 @@ return {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup {
-        defaults = {
-          layout_strategy = "horizontal",
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+      },
+      pickers = {
+        find_files = {
+          hidden = true
         },
-        pickers = {
-          find_files = {
-            hidden = true
-          },
-          live_grep = {
-            file_ignore_patterns = { 'node_modules', '.git', '.venv' },
-            additional_args = function(_)
-              return { "--hidden" }
-            end
-          },
+        live_grep = {
+          file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+          additional_args = function(_)
+            return { "--hidden" }
+          end
         },
-      }
+      },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
