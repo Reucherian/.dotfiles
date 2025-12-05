e8694cd3c1ef7fb77e8d595e06cdf3bbbeb09838
Reuben Cherian <cherianr@mskcc.org>
2025-12-04 21:34:25 -0500

    renaming plugins

---
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -0,0 +1,29 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		formatter_by_ft = {
+			python = { "ruff" },
+			lua = { "stylua" },
+		},
+		format_on_save = {
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 500,
+		},
+	},
+	keys = {
+		{
+			"<leader>gf",
+			function()
+				require("conform").format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 500,
+				})
+			end,
+			mode = { "n", "v" },
+			desc = "language format file or range",
+		},
+	},
+}
