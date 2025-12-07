5efd16c27465640f2b2ce77d6522be35af5d9685
Reuben Cherian <reuben.cherian@icloud.com>
2025-12-06 20:19:42 -0500

    using XDG to clean home directory

--- config.sh
+++ config.sh
@@ -9,6 +9,7 @@ if [ -d $HOME/.dotfiles ]; then
     stow nvim
     stow kanata
     stow btop
+    stow maven
 else
     echo "$HOME/.dotfiles does not exist.\n clone to the home folder instead"
 fi
